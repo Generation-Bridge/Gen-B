c0382b6a02dc5c62b7e93dad960bd209a36e32ec
Andy Wang <andy_wang@comcast.net>
2020-09-09 10:27:22 -0700

    graphql in progress, starting database schema

--- server/schema/schema.ts
+++ server/schema/schema.ts
@@ -1,3 +1,5 @@
+import {resolve} from 'path';
+
 const {
   GraphQLSchema,
   GraphQLObjectType,
@@ -9,12 +11,21 @@ const {
 
 //Sample data - to be deleted
 const helpersData = [
-  {id: 1, name: 'Hien', phone: '123456789', email: 'hien@hien.com'},
+  {
+    id: 1,
+    name: 'Hien',
+    phone: '123456789',
+    email: 'hien@hien.com',
+    password: 'hienhien',
+    // photo: null
+  },
   {
     id: 2,
     name: 'Justin',
     phone: '123456788',
     email: 'justin@justin.com',
+    password: 'justinjustin',
+    // photo: null
   },
 ];
 
@@ -24,27 +35,47 @@ const seniorsData = [
     name: 'Old Hien',
     phone: '123456789',
     email: 'oldhien@hien.com',
+    password: 'required',
+    ZIPCode: 90025,
   },
   {
     id: 2,
     name: 'Old Justin',
     phone: '123456781',
-    email: 'oldjustin@justin.com',
+    password: 'required',
   },
 ];
 
 const taskData = [
-  {id: 1, seniorID: 1, helperIDs: [1, 2], taskType: 'conversation', taskDescription: 'Need someone to talk to!'},
+  {
+    id: 1,
+    seniorID: 1,
+    helperIDs: [1, 2],
+    taskType: 'conversation',
+    taskDescription: 'Very lonely. Need someone to talk to!',
+    location: 'Phone call',
+  },
+  {
+    id: 2,
+    seniorID: 2,
+    helperIDs: [1, 2],
+    taskType: 'errand',
+    taskDescription: 'Need to buy rum',
+    location: "Old Justin's house",
+  },
 ];
 
+//Types - place in another folder
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   description: 'Specifies particular tasks',
   fields: {
-    id: {type: GraphQLInt!},
+    id: {type: GraphQLNonNull(GraphQLInt)},
     seniorID: {type: GraphQLNonNull(GraphQLInt)},
     helperIDs: {type: GraphQLList(GraphQLInt)},
     taskType: {type: GraphQLString},
+    taskDescription: {type: GraphQLString},
+    location: {type: GraphQLString},
   },
 });
 
@@ -56,6 +87,7 @@ const HelperType = new GraphQLObjectType({
     name: {type: GraphQLNonNull(GraphQLString)},
     phone: {type: GraphQLNonNull(GraphQLInt)},
     email: {type: GraphQLNonNull(GraphQLString)},
+    password: {type: GraphQLNonNull(GraphQLString)},
     tasks: {
       type: GraphQLList(TaskType),
       resolve: (helper: any) =>
@@ -71,7 +103,14 @@ const SeniorType = new GraphQLObjectType({
     id: {type: GraphQLNonNull(GraphQLInt)},
     name: {type: GraphQLNonNull(GraphQLString)},
     phone: {type: GraphQLNonNull(GraphQLInt)},
+    password: {type: GraphQLNonNull(GraphQLString)},
     email: {type: GraphQLString},
+    ZIPCode: {type: GraphQLInt},
+    tasks: {
+      type: GraphQLList(TaskType),
+      resolve: (senior: any) =>
+        taskData.filter(task => task.seniorID === senior.id),
+    },
   },
 });
 
@@ -79,6 +118,15 @@ const RootQueryType = new GraphQLObjectType({
   name: 'Query',
   description: 'Root Query',
   fields: () => ({
+    helper: {
+      type: HelperType,
+      description: 'Returns designated helper based on ID',
+      args: {
+        id: {type: GraphQLInt},
+      },
+      resolve: (parent: any, {id}: any) =>
+        helpersData.find(helper => helper.id === id),
+    },
     helpers: {
       type: new GraphQLList(HelperType),
       description: 'List of all helpers',
@@ -92,8 +140,38 @@ const RootQueryType = new GraphQLObjectType({
   }),
 });
 
+const RootMutationType = new GraphQLObjectType({
+  name: 'Mutation',
+  description: 'Root Mutation',
+  fields: () => ({
+    addHelper: {
+      type: HelperType,
+      description: 'Add a helper',
+      args: {
+        name: {type: GraphQLNonNull(GraphQLString)},
+        phone: {type: GraphQLNonNull(GraphQLInt)},
+        email: {type: GraphQLNonNull(GraphQLString)},
+        password: {type: GraphQLNonNull(GraphQLString)},
+      },
+      resolve: (parent: any, args: any) => {
+        const {name, phone, email, password} = args;
+        const newHelper = {
+          name,
+          phone,
+          email,
+          password,
+          id: helpersData.length + 1,
+        };
+        helpersData.push(newHelper);
+        return newHelper;
+      },
+    },
+  }),
+});
+
 const schema = new GraphQLSchema({
   query: RootQueryType,
+  mutation: RootMutationType,
 });
 
 module.exports = {schema};
