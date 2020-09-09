b1012fbf8900bcf303c1e83c095cc5e76cb82a2a
Hien Nguyen <hienqnguyen95@berkeley.edu>
2020-09-09 12:36:13 -0700

    added db

--- server/index.ts
+++ server/index.ts
@@ -10,3 +10,12 @@ const PORT = process.env.port || 3000;
 app.use('/graphql', graphqlHTTP({graphiql: true, schema}));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+// id	integer	 
+// type	integer	 
+// description	varchar(255)	 
+// senior	integer	 
+// deadline	time	 
+// completed	boolean
