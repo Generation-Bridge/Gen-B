da7d95982a2e7c8092069edafce628798baf5bbf
Justin Choo <kchoosun@gmail.com>
2020-09-09 20:38:53 -0700

    created bottom tab bar for junior dashboards
    Co-authored-by: johnmadrigal <johnmadrigal17@gmail.com>

---
+++ components/JuniorTickets.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const JuniorTickets = () => {
+  return (
+    <View>
+      <Text>Tickets!</Text>
+    </View>
+  );
+};
+
+export default JuniorTickets;
+
+const styles = StyleSheet.create({});
