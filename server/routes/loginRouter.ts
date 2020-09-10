1aa41dc52c72ed3c8386d7cb1b4612c42f840ef2
Andy Wang <andy_wang@comcast.net>
2020-09-09 20:24:32 -0700

    authentication logic complete for helpers

---
+++ server/routes/loginRouter.ts
@@ -0,0 +1,55 @@
+export {};
+const bcrypt = require('bcrypt');
+const express = require('express');
+const router = express.Router();
+const model = require('../schema/model');
+
+//Verification Middleware
+const verifyHelper = async (req: any, res: any, next: any) => {
+  const {phone, password} = req.body;
+  const queryText = 'SELECT * FROM helpers WHERE phone=$1';
+  const queryResult = await model.query(queryText, [phone]);
+  const [parsedResult] = queryResult.rows;
+
+  bcrypt.compare(
+    password,
+    parsedResult.password,
+    (err: any, isMatch: boolean) => {
+      if (err) return next(err);
+      if (isMatch) {
+        res.locals.id = parsedResult.id;
+        return next();
+      }
+    },
+  );
+};
+
+const verifySenior = async (req: any, res: any, next: any) => {
+  const {phone, password} = req.body;
+  const queryText = 'SELECT * FROM seniors WHERE phone=$1';
+  const queryResult = await model.query(queryText, [phone]);
+  const [parsedResult] = queryResult.rows;
+
+  bcrypt.compare(
+    password,
+    parsedResult.password,
+    (err: any, isMatch: boolean) => {
+      if (err) return next(err);
+      if (isMatch) {
+        res.locals.id = parsedResult.id;
+        return next();
+      }
+    },
+  );
+};
+
+//Router
+router.post('/helper', verifyHelper, (req: any, res: any) => {
+  res.status(200).json(res.locals.id);
+});
+
+router.post('/senior', verifySenior, (req: any, res: any) => {
+  res.status(200).json(res.locals.id);
+});
+
+module.exports = router;
